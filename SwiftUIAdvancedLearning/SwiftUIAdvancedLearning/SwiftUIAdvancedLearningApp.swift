1a2e0799970af0ccb59da0ed4ce3af4049c93d7c
Sarah Clark <sarahireneclark@gmail.com>
2024-05-10 13:00:54 -0500

    Initial project code

---
+++ SwiftUIAdvancedLearning/SwiftUIAdvancedLearning/SwiftUIAdvancedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAdvancedLearningApp.swift
+//  SwiftUIAdvancedLearning
+//
+//  Created by Sarah Clark on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAdvancedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIAdvancedLearning/SwiftUIAdvancedLearning/ViewModifierBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ViewModifierBootcamp.swift
+//  SwiftUIAdvancedLearning
+//
+//  Created by Sarah Clark on 5/10/24.
+//
+
+import SwiftUI
+
+struct ViewModifierBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewModifierBootcamp()
+}
